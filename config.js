1df22a64452a80ad6e6042619d11750e66994eff
MistyWorld <kalicem76@hotmail.fr>
2018-02-21 01:36:44 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,13 @@
+var api = 'http://gallion.ddns.net:3724';
+var blockTargetInterval = 220;
+var coinUnits = 100000000;
+var symbol = 'GMT';
+var refreshDelay = 30000;
+var networkStat = {
+    "gmt": [
+		["gallion.ddns.net/pool", "http://gallion.ddns.net:8117"]	
+		
+		
+    ]
+};
+
